33defabe71dd8fa1941a5ca55a0657d1ac15fa58
Michael Mogenson <michael.mogenson@gmail.com>
2020-03-29 16:42:48 -0400

    create fifo

--- writefifo/writefifo~.c
+++ writefifo/writefifo~.c
@@ -1,4 +1,13 @@
+#define _GNU_SOURCE
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "m_pd.h"
+
+#define FIFO_NAME "/home/mike/Music/fifo"
 
 static t_class *writefifo_tilde_class;
 
@@ -6,6 +15,7 @@ typedef struct _writefifo_tilde {
   t_object x_obj; // mandatory object
   t_sample f;     // dummy to enable signal on first inlet
   t_inlet *x_in2; // second signal inlet
+  int writefd;    // fifo file descriptor
 } t_writefifo_tilde;
 
 t_int *writefifo_tilde_perform(t_int *w) {
@@ -27,11 +37,32 @@ void writefifo_tilde_dsp(t_writefifo_tilde *x, t_signal **sp) {
           sp[0]->s_n);
 }
 
-void writefifo_tilde_free(t_writefifo_tilde *x) { inlet_free(x->x_in2); }
+void writefifo_tilde_free(t_writefifo_tilde *x) {
+  inlet_free(x->x_in2);
+  close(x->writefd); // also maybe delete fifo?
+}
 
 void *writefifo_tilde_new(void) {
   t_writefifo_tilde *x = (t_writefifo_tilde *)pd_new(writefifo_tilde_class);
   x->x_in2 = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
+
+  struct stat st;
+  if (!stat(FIFO_NAME, &st) && S_ISFIFO(st.st_mode)) {
+    /* fifo already exists */
+    post("fifo %s already exists", FIFO_NAME);
+  } else {
+    /* make fifo */
+    post("creating fifo %s", FIFO_NAME);
+    mkfifo(FIFO_NAME, 0666);
+  }
+
+  /* open fifo for read first so open for write succeeds */
+  int readfd = open(FIFO_NAME, O_RDONLY | O_NONBLOCK);
+  x->writefd = open(FIFO_NAME, O_WRONLY);
+  close(readfd);
+
+  /* set fifo size to minimize latency */
+  fcntl(x->writefd, F_SETPIPE_SZ, 0x2000);
   return (void *)x;
 }
 
